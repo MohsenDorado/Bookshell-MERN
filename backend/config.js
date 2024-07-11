93f80265c412b5bcb32a94594f0d0aca29bca414
MohsenDorado <161645984+MohsenDorado@users.noreply.github.com>
2024-07-11 17:07:10 +0330

    env

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,6 @@
 export const PORT = 5555;
 
-export const mongoDBURL =
-'mongodb+srv://wwwmking3000:d9wfngRrP1YrqDuu@bookstore.fthy7qn.mongodb.net/?retryWrites=true&w=majority&appName=Bookstore';
+export const mongoDBURL =process.env.DATABASE_URL;
 
 // Please create a free database for yourself.
 // This database will be deleted after tutorial
