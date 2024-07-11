e5f60edb59ced2c9bc8ce16fdc68085e29c55324
MohsenDorado <161645984+MohsenDorado@users.noreply.github.com>
2024-07-11 18:00:35 +0330

    get .then back

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,12 @@ app.use('/books', booksRoute);
 
 mongoose
   .connect(mongoDBURL)
-
+  .then(() => {
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
   .catch((error) => {
     console.log(error);
   });
