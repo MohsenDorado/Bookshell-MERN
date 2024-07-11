b1b4dfa4e7bdf3a19f8e17ae2ffb7299c71e3c64
MohsenDorado <161645984+MohsenDorado@users.noreply.github.com>
2024-07-11 17:20:59 +0330

    vercel

--- backend/index.js
+++ backend/index.js
@@ -7,19 +7,19 @@ import cors from 'cors';
 const app = express();
 
 // Middleware for parsing request body
-app.use(express.json());
+// app.use(express.json());
 
-// Middleware for handling CORS POLICY
-// Option 1: Allow All Origins with Default of cors(*)
-app.use(cors());
-// Option 2: Allow Custom Origins
-// app.use(
-//   cors({
-//     origin: 'http://localhost:3000',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type'],
-//   })
-// );
+app.use(cors(
+  {
+    origin:["https://mern-stack-bookshell.vercel.app"],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+     allowedHeaders: ['Content-Type'],
+    credentials:true
+
+  }
+
+
+))
 
 app.get('/', (request, response) => {
   console.log(request);
