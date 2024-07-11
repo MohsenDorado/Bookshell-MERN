2a597fb5ce828487ce6ef7098ef661192fd3623b
MohsenDorado <161645984+MohsenDorado@users.noreply.github.com>
2024-07-11 17:04:34 +0330

    first commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+'mongodb+srv://wwwmking3000:d9wfngRrP1YrqDuu@bookstore.fthy7qn.mongodb.net/?retryWrites=true&w=majority&appName=Bookstore';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
